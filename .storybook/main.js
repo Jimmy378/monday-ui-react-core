8a980a646d24262586c0b07992e2a4902879d59e
James Anderson <jamesanderson.contact@gmail.com>
2023-02-13 12:29:55 +1100

    add new addons

--- .storybook/main.js
+++ .storybook/main.js
@@ -35,7 +35,9 @@ const getAddons = () => {
     "storybook-addon-performance/register",
     "@storybook/addon-docs",
     "@storybook/addon-toolbars",
-    "@storybook/addon-actions"
+    "@storybook/addon-actions",
+    "@storybook/addon-measure",
+    "@storybook/addon-outline"
   ];
 
   if (process.env.NODE_ENV !== "production") {
